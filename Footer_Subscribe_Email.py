790870c512f6675239e57e18505bc617bdd49c17
Rupesh2001 <rmmahat2010@gmail.com>
2025-06-10 16:02:01 +0545

    updated

--- Footer_Subscribe_Email.py
+++ Footer_Subscribe_Email.py
@@ -35,7 +35,7 @@ def test_Footer_Subscribe_Email_Invalid(driver):
     driver.execute_script("window.scrollTo(0, 1000);") 
     time.sleep(2)
     Email_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Your email address']")))
-    Email_input.send_keys(faker.email())
+    Email_input.send_keys("@example.com")  # Invalid email format
     time.sleep(2)
     Subscribe_Btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
     Subscribe_Btn.click()
